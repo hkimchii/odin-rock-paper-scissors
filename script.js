8f73b13f4045b32deff6ab32d3ac023e56686cc3
hkimchii <hanskim.khk@gmail.com>
2021-11-08 21:31:16 +1300

    commit single round play

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-// Asks the user to pick a choice from rock, paper, or scissors
-let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase(); // Choice is converted to lower case
-let computerSelection = computerPlay();
-
 // Computer picks a random option from rock, paper, scissors
 function computerPlay() {
   let options = [
@@ -17,13 +11,30 @@ function computerPlay() {
   return randomOption;
 }
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
 // running of a round
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection){
     return playerSelection + " against " + computerSelection + "! It's a tie!";
   }
+  else if (playerSelection == "rock" && computerSelection == "paper"){
+    return "Rock against Paper! You lost!";
   }
-
-
-
-
+  else if (playerSelection == "rock" && computerSelection == "scissors"){
+    return "Rock against Scissors! You win!";
+  }
+  else if (playerSelection == "paper" && computerSelection == "rock"){
+    return "Paper against Rock! You win!";
+  }
+  else if (playerSelection == "paper" && computerSelection == "scissors"){
+    return "Paper against Scissors! You lose!";
+  }
+  else if (playerSelection == "scissors" && computerSelection == "rock"){
+    return "Scissors against Rock! You lose!";
+  }
+  else {
+    return "Scissors against Paper! You win!";
+  }
+  }
