20792882bcdc67fa772bcd2752d0baf0f1006647
hkimchii <hanskim.khk@gmail.com>
2021-11-23 13:18:20 +1300

    add game function

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-// Computer picks a random option from rock, paper, scissors
-function computerPlay() {
-  let options = [
-    "rock",
-    "paper",
-    "scissors"
-  ]
+function game() {
+  // Computer picks a random option from rock, paper, scissors
+  function computerPlay() {
+    let options = ["rock", "paper", "scissors"];
 
-  let randomOption = options[Math.floor(Math.random() * options.length)];
+    let randomOption = options[Math.floor(Math.random() * options.length)];
 
-  return randomOption;
+    return randomOption;
+  }
+
+  // running of a round
+  function playRound() {
+    let playerSelection = prompt("Choose your move").toLowerCase();
+    let computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+      return (
+        playerSelection + " against " + computerSelection + "! It's a tie!"
+      );
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+      return "Rock against Paper! You lose!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+      return "Rock against Scissors! You win!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+      return "Paper against Rock! You win!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+      return "Paper against Scissors! You lose!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+      return "Scissors against Rock! You lose!";
+    } else {
+      return "Scissors against Paper! You win!";
+    }
+  }
+
+  return playRound();
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-
-// running of a round
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection){
-    return playerSelection + " against " + computerSelection + "! It's a tie!";
-  }
-  else if (playerSelection == "rock" && computerSelection == "paper"){
-    return "Rock against Paper! You lost!";
-  }
-  else if (playerSelection == "rock" && computerSelection == "scissors"){
-    return "Rock against Scissors! You win!";
-  }
-  else if (playerSelection == "paper" && computerSelection == "rock"){
-    return "Paper against Rock! You win!";
-  }
-  else if (playerSelection == "paper" && computerSelection == "scissors"){
-    return "Paper against Scissors! You lose!";
-  }
-  else if (playerSelection == "scissors" && computerSelection == "rock"){
-    return "Scissors against Rock! You lose!";
-  }
-  else {
-    return "Scissors against Paper! You win!";
-  }
-  }
